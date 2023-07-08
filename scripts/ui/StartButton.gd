66b65ef61aac9dafa2f5601133b3ee620643b5af
Nick Fischer <fischer-nick@outlook.com>
2023-07-08 18:52:59 +0200

    Added Music

--- scripts/ui/StartButton.gd
+++ scripts/ui/StartButton.gd
@@ -1,5 +1,8 @@
 extends Button
 
+# TODO: Add MusicMute button
+# the music is good, but that's what everyone does first when opening up a game
+
 func _ready():
 	self.connect("pressed", _on_Button_pressed)
 
